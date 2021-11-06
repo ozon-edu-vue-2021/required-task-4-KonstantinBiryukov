bd987098eff34fcfa99f985592a939c4af620a0c
Konstantin Biriukov <kostya-leon@mail.ru>
2021-11-06 18:18:05 -0500

    init commit

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,11 +1,81 @@
 <template>
-  <div class="form"></div>
+  <div class="form">
+    <span>Личные данные</span>
+    <div class="row">
+      <label for="name">
+        Name
+      </label>
+      <input
+          v-model="formData.name"
+          type="text"
+          id="name"
+          placeholder="name"
+      >
+      <label for="surname">
+        Surname
+      </label>
+      <input
+          v-model="formData.surname"
+          type="text"
+          id="surname"
+          placeholder="surname"
+      >
+      <label for="patronymic">
+        Patronymic
+      </label>
+      <input
+          v-model="formData.patronymic"
+          type="text"
+          id="patronymic"
+          placeholder="patronymic"
+      >
+    </div>
+
+    <div class="row">
+      <label for="birthDate">
+        Date of birth
+      </label>
+      <input
+          v-model="formData.birthDate"
+          type="text"
+          id="birthDate"
+          placeholder="birthDate"
+      >
+    </div>
+
+    <div class="row">
+      <label for="email">
+        Email
+      </label>
+      <input
+          v-model="formData.email"
+          type="text"
+          id="email"
+          placeholder="test@gmail.com"
+      >
+    </div>
+  </div>
 </template>
 
 <script>
+import clickOutside from "vue-click-outside"
+
 export default {
+  name: "Form",
+  directives: {
+    clickOutside
+  },
   data() {
-    return {};
+    return {
+      formData: {
+        name: "",
+        surname: "",
+        patronymic: "",
+        birthDate: "",
+        email: "",
+        gender: "",
+      }
+    };
   },
   methods: {},
 };

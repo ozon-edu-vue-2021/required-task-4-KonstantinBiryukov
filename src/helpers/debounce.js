5a1e6d59b6e83f2196d89d09688befe93721dfe8
Konstantin Biriukov <kostya-leon@mail.ru>
2021-11-07 21:05:33 -0600

    remove vuetify required files

--- src/helpers/debounce.js
+++
@@ -1,9 +0,0 @@
-export function debounce(callback, delay) {
-    let timer = 0;
-    return function (...args) {
-        clearTimeout(timer);
-        timer = setTimeout(() => {
-            callback.apply(this, args)
-        }, delay)
-    }
-}

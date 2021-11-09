c53aad40519050d731fc47dca7ad67c3fc66667b
Konstantin Biriukov <kostya-leon@mail.ru>
2021-11-09 10:34:19 -0600

    add json console output and debounce for citizenship input

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -43,7 +43,7 @@
         <v-col cols="12" md="10">
           <v-text-field
               v-model="personalData.birthDate"
-              :rules="[rules.required, rules.birthDate]"
+              :rules="[rules.required, rules.date]"
               :counter="10"
               label="Date of birth"
               placeholder="DD.MM.YYYY"
@@ -92,7 +92,7 @@
         <v-autocomplete
             :items="citizenships"
             label="Citizenship"
-            v-model="passportData.citizenship"
+            :search-input.sync="searchInput"
             dense
             outlined
         ></v-autocomplete>
@@ -101,106 +101,108 @@
       <div v-if="!passportData.citizenship">
 
       </div>
-
-      <div v-else-if="passportData.citizenship === 'Russia'">
-        <v-row>
-          <v-col cols="12" md="4">
-            <v-text-field
-                v-model="passportData.passportSerial"
-                :counter="4"
-                :rules="[rules.passportSerial, rules.required]"
-                label="Passport Serial"
-                required
-                solo
-            ></v-text-field>
-          </v-col>
-
-          <v-col cols="12" md="4">
-            <v-text-field
-                v-model="passportData.passportNumber"
-                :counter="6"
-                :rules="[rules.passportNumber, rules.required]"
-                label="Passport Number"
-                required
-                solo
-            ></v-text-field>
-          </v-col>
-
-          <v-col cols="12" md="4">
-            <v-text-field
-                v-model="passportData.issueDate"
-                :counter="10"
-                label="Issue date"
-                required
-                solo
-            ></v-text-field>
-          </v-col>
-        </v-row>
-      </div>
-
       <div v-else>
-        <v-row>
-          <v-col cols="12" md="6">
-            <v-text-field
-                v-model="foreignPassport.latinName"
-                :counter="20"
-                :rules="[rules.required, rules.english]"
-                label="Latin Name"
-                required
-                solo
-            ></v-text-field>
-          </v-col>
+        <div v-if="passportData.citizenship=== 'Russia'">
+          <v-row>
+            <v-col cols="12" md="4">
+              <v-text-field
+                  v-model="passportData.passportSerial"
+                  :counter="4"
+                  :rules="[rules.passportSerial, rules.required]"
+                  label="Passport Serial"
+                  required
+                  solo
+              ></v-text-field>
+            </v-col>
 
-          <v-col cols="12" md="6">
-            <v-text-field
-                v-model="foreignPassport.latinSurname"
-                :rules="[rules.required, rules.english]"
-                :counter="20"
-                label="Latin Surname"
-                required
-                solo
-            ></v-text-field>
-          </v-col>
-        </v-row>
+            <v-col cols="12" md="4">
+              <v-text-field
+                  v-model="passportData.passportNumber"
+                  :counter="6"
+                  :rules="[rules.passportNumber, rules.required]"
+                  label="Passport Number"
+                  required
+                  solo
+              ></v-text-field>
+            </v-col>
 
-        <v-row>
-          <v-col cols="12" md="4">
-            <v-text-field
-                v-model="passportData.passportNumber"
-                :counter="15"
-                label="Passport Number"
-                required
-                solo
-            ></v-text-field>
-          </v-col>
+            <v-col cols="12" md="4">
+              <v-text-field
+                  v-model="passportData.issueDate"
+                  :rules="[rules.date]"
+                  :counter="10"
+                  label="Issue date"
+                  required
+                  solo
+              ></v-text-field>
+            </v-col>
+          </v-row>
+        </div>
 
-          <v-col
-              cols="12"
-              md="4"
-          >
-            <v-select
-                :items="citizenships"
-                label="Country of Issue"
-                v-model="foreignPassport.countryIssue"
-                dense
-                outlined
-            ></v-select>
-          </v-col>
+        <div v-else>
+          <v-row>
+            <v-col cols="12" md="6">
+              <v-text-field
+                  v-model="foreignPassport.latinName"
+                  :counter="20"
+                  :rules="[rules.required, rules.english]"
+                  label="Latin Name"
+                  required
+                  solo
+              ></v-text-field>
+            </v-col>
 
-          <v-col
-              cols="12"
-              md="4"
-          >
-            <v-select
-                :items="passportTypes"
-                label="Passport Type"
-                v-model="foreignPassport.passportType"
-                dense
-                outlined
-            ></v-select>
-          </v-col>
+            <v-col cols="12" md="6">
+              <v-text-field
+                  v-model="foreignPassport.latinSurname"
+                  :rules="[rules.required, rules.english]"
+                  :counter="20"
+                  label="Latin Surname"
+                  required
+                  solo
+              ></v-text-field>
+            </v-col>
+          </v-row>
 
-        </v-row>
+          <v-row>
+            <v-col cols="12" md="4">
+              <v-text-field
+                  v-model="passportData.passportNumber"
+                  :counter="15"
+                  label="Passport Number"
+                  required
+                  solo
+              ></v-text-field>
+            </v-col>
+
+            <v-col
+                cols="12"
+                md="4"
+            >
+              <v-select
+                  :items="citizenships"
+                  label="Country of Issue"
+                  v-model="foreignPassport.countryIssue"
+                  dense
+                  outlined
+              ></v-select>
+            </v-col>
+
+            <v-col
+                cols="12"
+                md="4"
+            >
+              <v-select
+                  :items="passportTypes"
+                  label="Passport Type"
+                  v-model="foreignPassport.passportType"
+                  dense
+                  outlined
+              ></v-select>
+            </v-col>
+
+          </v-row>
+        </div>
       </div>
 
       <div>
@@ -255,6 +257,7 @@
 <script>
 import citizenships from "@/assets/data/citizenships.json";
 import passportTypes from "@/assets/data/passport-types.json";
+import {debounce} from "@/helpers/debounce";
 
 export default {
   name: "Form",
@@ -309,26 +312,42 @@ export default {
           const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
           return pattern.test(value) || `Email is not valid`;
         },
-        birthDate: value => {
+        date: value => {
           const pattern = /^([0-9]{2}).([0-9]{2}).([0-9]{4})$/;
           let isValid = pattern.test(value);
           if (isValid && new Date(value) >= new Date()) {
-            return `Date of birth cannot be greater than the current time`;
+            return `Date cannot be greater than the current time`;
           }
-          return isValid || `Date of birth is not valid`;
+          return isValid || `Date  is not valid`;
         }
-      }
+      },
+      searchInput: ""
     };
   },
   created() {
     this.citizenships = citizenships.map(citizenship => citizenship.nationality);
     this.passportTypes = passportTypes.map(passportType => passportType.type);
   },
+  watch: {
+    searchInput: debounce(function (newVal) {
+      if (newVal !== null) {
+        this.passportData.citizenship = newVal;
+        console.log(newVal);
+      }
+    }, 2000)
+  },
   methods: {
     validate() {
       const isValid = this.$refs.form.validate();
-      if (isValid) alert("Form is submitted");
-    }
+      if (isValid) {
+        console.log("personal data", this.personalData);
+        console.log("passport data", this.passportData);
+        console.log("foreign passport", this.foreignPassport);
+        console.log("name change", this.changeName);
+
+        alert("Form is submitted");
+      }
+    },
   },
 };
 </script>
